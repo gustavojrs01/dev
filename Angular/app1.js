d8684c6d173b6339919e300b087ce379ec852098
gustavojrs01 <gustavojrs01@gmail.com>
2019-12-04 11:22:48 -0300

    1

--- Angular/app1.js
+++ Angular/app1.js
@@ -1,2 +1,6 @@
-var nombre = "Gustavo Rodriguez";
-console.log(nombre);
+var nombre = {
+    nombre:"Gustavo",
+    apellido: "Rodriguez",
+    edad: 32
+};
+console.log("Su nombre es: "+nombre.nombre+" y su apellido es: "+nombre.apellido+". Tiene "+nombre.edad+" años");

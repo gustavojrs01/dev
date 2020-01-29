40718acaca5d09be03e1f116a33f80dca3975295
gustavojrs01 <gustavojrs01@gmail.com>
2020-01-29 18:28:56 -0300

    CRUD

--- Angular/crud-nodejs-mongodb/src/routes/index.js
+++ Angular/crud-nodejs-mongodb/src/routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const Task = require('../models/task');
+
 router.get('/', (req, res)=>{
     res.render("index");
+});
+
+router.post('/add', (req, res)=>{
+    console.log(req.body);
+    res.send('Received');
+    
 });
 
 module.exports = router;

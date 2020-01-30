08fc52a3935bf99925e70d7bc907d0c68f07e722
Gustavo Rodriguez <gustavojrs01@gmail.com>
2020-01-30 01:43:38 -0600

    Crud

--- Angular/crud-nodejs-mongodb/src/routes/index.js
+++ Angular/crud-nodejs-mongodb/src/routes/index.js
@@ -3,14 +3,50 @@ const router = express.Router();
 
 const Task = require('../models/task');
 
-router.get('/', (req, res)=>{
-    res.render("index");
+router.get('/', async (req, res)=>{
+    const tasks = await Task.find();
+    // console.log(tasks);
+    res.render("index", {
+        tasks
+    });
 });
 
-router.post('/add', (req, res)=>{
-    console.log(req.body);
-    res.send('Received');
+router.post('/add', async (req, res)=>{
+    // console.log(new Task(req.body));
+    // console.log(req.body);
+    const task = new Task(req.body);
+    await task.save();
+    res.redirect('/');
     
+});
+
+router.get('/delete/:id', async (req, res)=>{
+    const {id} = req.params;
+    await Task.remove({_id:id});
+    res.redirect('/');
+});
+
+router.get('/turn/:id', async (req, res)=>{
+    const {id} = req.params;
+    const task = await Task.findById(id);
+    task.status = !task.status;
+    await task.save();
+    console.log('task')
+    res.redirect('/');
+} );
+
+router.get('/edit/:id', async(req,res)=>{
+    const {id} = req.params;
+    const task = await Task.findById(id);
+    res.render('edit', {
+        task
+    });
+});
+
+router.post('/edit/:id', async (req, res)=>{
+    const {id} = req.params;
+    await Task.update({_id:id}, req.body);
+    res.redirect('/');
 });
 
 module.exports = router;

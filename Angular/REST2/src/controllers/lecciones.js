83eda2513975c5e7c6cf6fe9b37ca055a60cc769
gustavojrs01 <gustavojrs01@gmail.com>
2020-01-31 18:45:58 -0300

    CRUD

--- Angular/REST2/src/controllers/lecciones.js
+++ Angular/REST2/src/controllers/lecciones.js
@@ -16,16 +16,27 @@ module.exports = {
         const leccion = await Leccion.findById(leccionId);
         res.status(200).json(leccion); 
     },
+    getLeccionCodigo: async (req, res, next)=>{
+        const { codigoLeccion } = req.params;
+        const leccion = await Leccion.findOne({"codigo":codigoLeccion});
+        res.status(200).json(leccion); 
+    },
     replaceLeccion: async (req, res, next)=>{
         const { leccionId } = req.params;
         const nuevaLeccion = req.body;
         const viejaLeccion = await Leccion.findByIdAndUpdate(leccionId, nuevaLeccion);
-        res.status(200).json({success: true}); 
+        res.status(200).json({success: true});        
     },
     updateLeccion: async (req, res, next)=>{
         const { leccionId } = req.params;
         const nuevaLeccion = req.body;
         const viejaLeccion = await Leccion.findByIdAndUpdate(leccionId, nuevaLeccion);
+        res.status(200).json({success: true}); 
+    },
+    deleteLeccion: async (req, res, next)=>{
+        const { leccionId } = req.params;
+        const nuevaLeccion = req.body;
+        await Leccion.findByIdAndRemove(leccionId);
         res.status(200).json({success: true}); 
     }
 
--- Angular/REST2/src/routes/lecciones.js
+++ Angular/REST2/src/routes/lecciones.js
@@ -5,10 +5,18 @@ const router = require('express-promise-router')();
 
 const {
     index,
-    setLeccion
+    setLeccion,
+    getLeccion,
+    getLeccionCodigo,
+    replaceLeccion,
+    deleteLeccion
 } = require('../controllers/lecciones');
 
 router.get('/', index);
 router.post('/', setLeccion);
+router.get('/:leccionId', getLeccion);
+router.get('/codigo/:codigoLeccion', getLeccionCodigo);
+router.put('/:leccionId', replaceLeccion);
+router.delete('/:leccionId', deleteLeccion);
 
 module.exports = router;
